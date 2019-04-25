9d6fc5330c58b7927158e4054df1527e6d267bbd
unknown <doublenegative@protonmail.com>
2019-04-25 11:22:58 +0200

    changed folder/package capital letterrs. Fixed navbar

---
+++ FogProject/src/main/webapp/inclusions/NavBar.jsp
@@ -0,0 +1,33 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Navigation Bar</title>    
+        <!--<link rel="stylesheet" type="text/css" href="altcss.css">-->
+        <meta name="viewport" content="width=device-width, initial-scale=1">       
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    </head>
+
+    <body>
+        <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="/FogProject/">Fog</a>
+                </div>
+<!--                <ul class="nav navbar-nav">
+                    <li class="active"><a href="/Cupcake/c/shopping">Home</a></li>
+                    <li><a href="/Cupcake/c/possibilities">Menu</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="/Cupcake/c/showuser"><span class="glyphicon glyphicon-user"></span> Customer Page</a></li>
+                    <li><a href="/Cupcake/SessionExit.jsp"><span class="glyphicon glyphicon-log-out"></span> Log out</a></li>
+                </ul>-->
+            </div>
+        </nav>
+    </body>
+
+</html>
---
+++ FogProject/src/test/java/data/help_classes/CarportTest.java
@@ -0,0 +1,76 @@
+/*
+ */
+package data.help_classes;
+
+import data.help_classes.Roof;
+import data.help_classes.Carport;
+import data.help_classes.Shed;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author Simon Asholt Norup
+ */
+public class CarportTest {
+    
+    private final Shed shed;
+    private final Roof roof;
+
+    public CarportTest() {
+        this.shed = new Shed(210, 240, "Plastic");
+        this.roof = new Roof("Eternittag B6 - Teglrød", 25);
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getLength method, of class Carport.
+     */
+    @Test (expected = IllegalArgumentException.class)
+    public void testGetLength() {
+        Carport carport = new Carport(200, 300, 250, roof, shed);
+    }
+
+    /**
+     * Test of getWidth method, of class Carport.
+     */
+    @Test (expected = IllegalArgumentException.class)
+    public void testGetWidth() {
+        Carport carport = new Carport(300, 0, 250, roof, shed);
+    }
+
+    /**
+     * Test of getHeight method, of class Carport.
+     */
+    @Test (expected = IllegalArgumentException.class)
+    public void testGetHeight() {
+        Carport carport = new Carport(300, 300, -200, roof, shed);
+    }
+
+    /**
+     * Test of getRoof method, of class Carport.
+     */
+    @Test (expected = IllegalArgumentException.class)
+    public void testGetRoof() {
+        Carport carport = new Carport(300, 300, 250, null, shed);
+    }
+    
+}
