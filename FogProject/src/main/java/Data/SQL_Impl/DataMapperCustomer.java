15c4371d539d66856a302bd00c37fe33596e7f50
Svense <emil.svensmark@gmail.com>
2019-04-25 10:58:02 +0200

    Changing Branch

---
+++ FogProject/src/main/java/Data/SQL_Impl/DataMapperCustomer.java
@@ -0,0 +1,37 @@
+package Data.SQL_Impl;
+
+import Data.help_classes.Carport;
+import Data.help_classes.Customer;
+import Data.help_classes.Request;
+import Data.help_classes.Roof;
+import Data.help_classes.Shed;
+
+public class DataMapperCustomer implements DataMapperCustomerInterface{
+
+    @Override
+    public void createCustomer(Customer customer) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void createRequest(Request request) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void createCarport(Carport carport) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void createRoof(Roof roof) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void createShed(Shed shed) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+}
---
+++ FogProject/src/main/java/Data/SQL_Impl/DataMapperCustomerInterface.java
@@ -0,0 +1,22 @@
+package Data.SQL_Impl;
+
+import Data.help_classes.*;
+
+/**
+ *
+ * @author Emil PC
+ */
+public interface DataMapperCustomerInterface {
+    
+    
+    public void createCustomer(Customer customer);
+    
+    public void createRequest(Request request);
+    
+    public void createCarport(Carport carport);
+    
+    public void createRoof(Roof roof);
+    
+    public void createShed(Shed shed);
+    
+}
