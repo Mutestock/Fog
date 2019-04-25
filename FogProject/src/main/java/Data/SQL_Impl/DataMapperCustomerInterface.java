d3fce2f0f7945bce55beb78b5ec00a82d1466456
Svense <emil.svensmark@gmail.com>
2019-04-25 13:04:47 +0200

    datamapper for customer

--- FogProject/src/main/java/Data/SQL_Impl/DataMapperCustomerInterface.java
+++ FogProject/src/main/java/Data/SQL_Impl/DataMapperCustomerInterface.java
@@ -11,12 +11,21 @@ public interface DataMapperCustomerInterface {
     
     public void createCustomer(Customer customer);
     
-    public void createRequest(Request request);
+    public void createRequest(Request request, Customer customer);
     
-    public void createCarport(Carport carport);
+    public void createCarport(Carport carport, Shed shed, Roof roof);
     
     public void createRoof(Roof roof);
     
     public void createShed(Shed shed);
+ 
+    
+    public int readCarportId(Carport carport);
+    
+    public int readCustomerId(Customer customer);
+    
+    public int readShedId(Shed shed);
+    
+    public int readRoofId(Roof roof);
     
 }
