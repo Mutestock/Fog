07c5fd4240b570e55c56269385cdfb367e4af22e
wormple12 <wormple12@hotmail.com>
2019-04-25 11:39:55 +0200

    first unfinished version of facade between presentation and logic created.

---
+++ FogProject/src/main/java/Logic/PresentationToLogic.java
@@ -0,0 +1,19 @@
+
+package Logic;
+
+import Data.CustomExceptions.DataAccessException;
+import Data.help_classes.Customer;
+import Data.help_classes.Request;
+
+/**
+ * @author Simon Asholt Norup
+ */
+public interface PresentationToLogic {
+    
+    public void sendRequest(Request request, Customer customer) throws DataAccessException;
+    
+    // methods for getting available widths, lengths, roof types, etc, etc
+    
+    // method for creating and/or showing SVG file for carport sketch drawing
+
+}
